682e48d4cdeb67b037f2e45bab9c0ddaa2669d7a
diabolo-dan <diabolo.dan@gmail.com>
2018-11-19 12:46:37 +0000

    pink_flamingo -- Refactor into GenericFizzBuzz class
    This maintains the relationship between normal FizzBuzz and Flamingo, allowing the implementation to both be available without repetition, whilst also making changes/new conditions rules easy to implement

--- pink_flamingo/pink_flamingo.py
+++ pink_flamingo/pink_flamingo.py
@@ -4,45 +4,37 @@ def is_fib(n):
     x = 5 * n**2
     return gmpy.is_square(x + 4) or gmpy.is_square(x - 4)
 
-def fizz_buzz(n):
-    if n%3 + n%5 == 0:
-        return "fizzbuzz"
-    elif n % 3 == 0:
-        return "fizz"
-    elif n % 5 == 0:
-        return "buzz"
-    return str(n)
-
-def flamingo_old(n):
-    fizz_buzz_output = fizz_buzz(n)
-    if is_fib(n):
-        if fizz_buzz_output == 'fizzbuzz':
-            return 'pink flamingo'
-        else:
-            return 'flamingo'
-    else:
-        return fizz_buzz_output
-
-CONDITIONS = [
+FIZZ_BUZZ_CONDITIONS = [
     lambda x: x % 3 == 0,
     lambda x: x % 5 == 0,
-    lambda x: is_fib(x)
 ]
-RULES = [
+FLAMINGO_CONDITIONS = FIZZ_BUZZ_CONDITIONS + [lambda x: is_fib(x)]
+FIZZ_BUZZ_RULES = [
     ([0], 'fizz'),
     ([1], 'buzz'),
     ([0,1], 'fizzbuzz'),
+]
+FLAMINGO_RULES = FIZZ_BUZZ_RULES + [
     ([2], 'flamingo'),
     ([0, 1, 2], 'pink flamingo')
 ]
 
-def flamingo(n):
-    matching_conditions = [condition(n) for condition in conditions]
-    for condition_indexes, output in reversed(rules):
-        if all([matching_conditions[i] for i in condition_indexes]):
-            return  output
-    else:
-        return str(n)
+class GenericFizzBuzz(object):
+    def __init__(self, conditions, rules):
+        self.conditions = conditions
+        self.rules = rules
+
+    def __call__(self, n):
+        matching_conditions = [condition(n) for condition in self.conditions]
+        for condition_indexes, output in reversed(self.rules):
+            if all([matching_conditions[i] for i in condition_indexes]):
+                return  output
+        else:
+            return str(n)
+
+
+fizz_buzz = GenericFizzBuzz(FIZZ_BUZZ_CONDITIONS, FIZZ_BUZZ_RULES)
+flamingo = GenericFizzBuzz(FLAMINGO_CONDITIONS, FLAMINGO_RULES)
 
 def main():
     for i in range(1, 100):
