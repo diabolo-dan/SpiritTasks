a67544f182ffc0875b3fa66e4b3bd88b3148acdd
diabolo-dan <diabolo.dan@gmail.com>
2018-11-19 12:37:55 +0000

    pink_flamingo -- Reimplement Flamingo to check all conditions directly
    As opposed to relying on fizzbuzz implementation.  This is a more general solution, but for the problem at hand might give up more in clarity than it gains in implementation.

--- pink_flamingo/pink_flamingo.py
+++ pink_flamingo/pink_flamingo.py
@@ -13,7 +13,7 @@ def fizz_buzz(n):
         return "buzz"
     return str(n)
 
-def flamingo(n):
+def flamingo_old(n):
     fizz_buzz_output = fizz_buzz(n)
     if is_fib(n):
         if fizz_buzz_output == 'fizzbuzz':
@@ -23,6 +23,26 @@ def flamingo(n):
     else:
         return fizz_buzz_output
 
+CONDITIONS = [
+    lambda x: x % 3 == 0,
+    lambda x: x % 5 == 0,
+    lambda x: is_fib(x)
+]
+RULES = [
+    ([0], 'fizz'),
+    ([1], 'buzz'),
+    ([0,1], 'fizzbuzz'),
+    ([2], 'flamingo'),
+    ([0, 1, 2], 'pink flamingo')
+]
+
+def flamingo(n):
+    matching_conditions = [condition(n) for condition in conditions]
+    for condition_indexes, output in reversed(rules):
+        if all([matching_conditions[i] for i in condition_indexes]):
+            return  output
+    else:
+        return str(n)
 
 def main():
     for i in range(1, 100):
