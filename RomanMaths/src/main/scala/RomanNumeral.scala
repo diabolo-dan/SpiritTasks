2093f3868a1540f7d312849ad17e39d880558044
diabolo-dan <diabolo.dan@gmail.com>
2018-11-20 09:49:54 +0000

    RomanMaths -- Add base numeral mapping

--- RomanMaths/src/main/scala/RomanNumeral.scala
+++ RomanMaths/src/main/scala/RomanNumeral.scala
@@ -3,8 +3,19 @@ class RomanNumeral(val value: Int) {
 }
 
 object RomanNumeral {
+  //TODO: Verify D/L are correct.
+  private val baseNumerals = Map(
+    "I" -> 1,
+    "V" -> 5,
+    "X" -> 10,
+    "L" -> 50,
+    "C" -> 100,
+    "D" -> 500,
+    "M" -> 1000
+  )
+
   def apply(numeral: String): RomanNumeral = {
-   return new RomanNumeral(1)
+   return new RomanNumeral(baseNumerals.getOrElse(numeral, 1))
   }
 
 }
