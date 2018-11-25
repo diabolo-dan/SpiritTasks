bcdf342004e55c69b68a1366f114ee577bfd6f7a
diabolo-dan <diabolo.dan@gmail.com>
2018-11-25 15:52:38 +0000

    RomanMaths -- add operators for minus, times, divides to parser

--- RomanMaths/src/main/scala/NumeralParser.scala
+++ RomanMaths/src/main/scala/NumeralParser.scala
@@ -4,4 +4,8 @@ trait NumeralParser extends RegexParsers {
   def romanNumeral: Parser[Int] = """[A-Z]+""".r ^^ {RomanNumeral(_).get.value}
 
   def plus: Parser[Int] = romanNumeral ~ "+" ~ romanNumeral ^^ {case a~"+"~b => a + b}
+  def minus: Parser[Int] = romanNumeral ~ "-" ~ romanNumeral ^^ {case a~"-"~b => a - b}
+  def times: Parser[Int] = romanNumeral ~ "*" ~ romanNumeral ^^ {case a~"*"~b => a * b}
+  def divides: Parser[Int] = romanNumeral ~ "/" ~ romanNumeral ^^ {case a~"/"~b => a / b}
+
 }
