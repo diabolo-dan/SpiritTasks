3198e8e17e02b0f559ab66efbd23564219213794
diabolo-dan <diabolo.dan@gmail.com>
2018-11-25 16:49:36 +0000

    RomanMaths -- Implement proper parsing based on sample implementation

--- RomanMaths/src/main/scala/NumeralParser.scala
+++ RomanMaths/src/main/scala/NumeralParser.scala
@@ -1,11 +1,23 @@
-import scala.util.parsing.combinator.RegexParsers
+import scala.util.parsing.combinator.{RegexParsers, PackratParsers}
 
-trait NumeralParser extends RegexParsers {
+trait NumeralParser extends RegexParsers with PackratParsers {
+  /*
+  Parser based on the code found in the answer to:
+   https://stackoverflow.com/questions/11533547/operator-precedence-with-scala-parser-combinators
+  This was the most straightforward parser that I could find, and is at least as clear as I could come up with
+  independently
+   */
+
   def romanNumeral: Parser[Int] = """[A-Z]+""".r ^^ {RomanNumeral(_).get.value}
 
-  def plus: Parser[Int] = romanNumeral ~ "+" ~ romanNumeral ^^ {case a~"+"~b => a + b}
-  def minus: Parser[Int] = romanNumeral ~ "-" ~ romanNumeral ^^ {case a~"-"~b => a - b}
-  def times: Parser[Int] = romanNumeral ~ "*" ~ romanNumeral ^^ {case a~"*"~b => a * b}
-  def divides: Parser[Int] = romanNumeral ~ "/" ~ romanNumeral ^^ {case a~"/"~b => a / b}
+  def expr:   Parser[Int]        = term ~ rep(plus | minus) ^^ {case a ~ b => (a /: b)((acc,f) => f(acc))}
+  def plus:   Parser[Int => Int] = "+" ~ term               ^^ {case "+" ~ b => _ + b}
+  def minus:  Parser[Int => Int] = "-" ~ term               ^^ {case "-" ~ b => _ - b}
+
+  def term:   Parser[Int]        = factor ~ rep(times | divide) ^^ {case a ~ b => (a /: b)((acc,f) => f(acc))}
+  def times:  Parser[Int => Int] = "*" ~ factor                 ^^ {case "*" ~ b => _ * b }
+  def divide: Parser[Int => Int] = "/" ~ factor                 ^^ {case "/" ~ b => _ / b}
+
+  def factor: Parser[Int] = romanNumeral
 
 }
--- RomanMaths/src/test/scala/TestParsing.scala
+++ RomanMaths/src/test/scala/TestParsing.scala
@@ -5,18 +5,21 @@ import scala.util.Try
 class TestParsing extends FreeSpec with NumeralParser with Matchers with AppendedClues with TryValues {
 
   val parser = new NumeralParser {}
+  def parseExpression(expression: String): Int = {
+    val result = parser.parseAll(parser.expr, expression)
+    result shouldBe a[Success[_]]
+    result.get
+  }
 
   "A string parsed as a numeral" - {
     "when valid" - {
       "should parse to a roman numeral" in {
-        val result = parser.parse(parser.romanNumeral, "XV")
-        result shouldBe a [Success[_]]
-        result.get shouldBe 15
+        parseExpression("XV") shouldBe 15
       }
     }
     "when invalid" - {
       "should raise an error" in {
-        val result = Try(parser.parse(parser.romanNumeral, "VX"))
+        val result = Try(parseExpression("VX"))
         result.failure.exception shouldBe a [IllegalArgumentException]
       }
     }
@@ -25,30 +28,44 @@ class TestParsing extends FreeSpec with NumeralParser with Matchers with Appende
   "A string parsed with an operator" - {
     "for an addition operator" - {
       "should return the summed value" in {
-        val result = parser.parse(parser.plus, "XV + IV")
-        result shouldBe a [Success[_]]
-        result.get shouldBe 19
+        parseExpression("XV + IV") shouldBe 19
       }
     }
     "for a subtraction operator" - {
       "should return the subtracted value" in {
-        val result = parser.parse(parser.minus, "XV - IV")
-        result shouldBe a[Success[_]]
-        result.get shouldBe 11
+        parseExpression("XV - IV") shouldBe 11
       }
     }
     "for a multiplication operator" - {
       "should return the multiplied value" in {
-        val result = parser.parse(parser.times, "XV * II")
-        result shouldBe a [Success[_]]
-        result.get shouldBe 30
+        parseExpression("XV * II") shouldBe 30
       }
      }
     "for an division operator" - {
       "should return the integer divided value" in {
-        val result = parser.parse(parser.divides, "XV / IV")
-        result shouldBe a[Success[_]]
-        result.get shouldBe 3
+        parseExpression("XV / IV") shouldBe 3
+      }
+    }
+    "for a composite statement" - {
+      "with additions" - {
+        "should parse correctly" in {
+          parseExpression("XV + IV + XX") shouldBe 39
+        }
+      }
+      "with subtractions" - {
+        "should parse correctly" in {
+         parseExpression("XX - V - II")  shouldBe 13
+        }
+      }
+      "with multiplications" - {
+        "should parse correctly" in {
+          parseExpression("V * II * IV") shouldBe 40
+        }
+      }
+      "with divisions" - {
+        "should parse correctly" in {
+          parseExpression("XX / II / III") shouldBe 3
+        }
       }
     }
   }
