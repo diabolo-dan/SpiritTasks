cdf7c4d121db9fea11feb78c9f3db4959dda2335
diabolo-dan <diabolo.dan@gmail.com>
2018-11-27 10:26:10 +0000

    RomanMaths -- Restrict Access-Control-Allow-Methods

--- RomanMaths/src/main/scala/RomanMathServer.scala
+++ RomanMaths/src/main/scala/RomanMathServer.scala
@@ -13,7 +13,7 @@ object RomanMathServer extends cask.MainRoutes {
 
   def enableCorsHeaders : Seq[(String, String)] = Seq(
     "Access-Control-Allow-Origin" -> "*"
-    , "Access-Control-Allow-Methods" -> "OPTIONS, GET, POST, PUT, DELETE, HEAD"   // OPTIONS for pre-flight
+    , "Access-Control-Allow-Methods" -> "OPTIONS, GET"   // OPTIONS for pre-flight
     , "Access-Control-Allow-Headers" -> "Accept, Content-Type, Origin, X-Json, X-Prototype-Version, X-Requested-With" //, "X-My-NonStd-Option"
     , "Access-Control-Allow-Credentials" -> "true"
   )
