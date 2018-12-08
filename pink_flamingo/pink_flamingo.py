9da3518637158d42685140b20dc57a0300d7d22c
diabolo-dan <diabolo.dan@gmail.com>
2018-12-08 22:48:50 +0000

    pink_flamingo -- simplify implementation by removing conditions.
    These added more complexity than they removed in duplication.  I've also
    removed the reversing of the rules, using early first priority.

--- pink_flamingo/pink_flamingo.py
+++ pink_flamingo/pink_flamingo.py
@@ -2,52 +2,43 @@
 import gmpy
 
 class GenericFizzBuzz(object):
-    """Takes a list of conditions, as a dictionary with a name index, and a list of rules from sets of those conditions (by name) to desired output string.  Returns a callable which, when supplied an integer n, will return the output string determined by the given rules.  A later rule will take precedence over an earlier rule.  If no rule matches, the input n will be returned as a string.
+    """Takes a list of rules from conditions (as a boolean function on a number) to desired output string.  Returns a callable which, when supplied an integer n, will return the output string determined by the given rules.  An earlier rule will take precedence over an later rule.  If no rule matches, the input n will be returned as a string.
 
-    E.g. for fizzbuzz conditions are
-        fizz: n%3 == 0
-        buzz: n%5 == 0
-    rules are
-        fizz -> 'fizz',
-        buzz -> 'buzz',
-        fizz + buzz -> 'fizzbuzz'
+    E.g. for fizzbuzz rules are:
+        x % 3 == 0  -> 'fizz',
+        x % 5 == 0  -> 'buzz',
+        x % 15 == 0 -> 'fizzbuzz'
     """
 
-    def __init__(self, conditions, rules):
-        self.conditions = conditions
+    def __init__(self, rules):
         self.rules = rules
 
     def __call__(self, n):
-        matching_conditions = {name: condition(n) for name, condition in self.conditions.items()}
-        for condition_names, output in reversed(self.rules):
-            if all([matching_conditions[i] for i in condition_names]):
+        for condition, output in self.rules:
+            if condition(n):
                 return  output
         else:
             return str(n)
 
 
-FIZZ_BUZZ_CONDITIONS = {
-        'fizz': lambda x: x % 3 == 0,
-        'buzz': lambda x: x % 5 == 0,
-}
 FIZZ_BUZZ_RULES = [
-    ({'fizz'}, 'fizz'),
-    ({'buzz'}, 'buzz'),
-    ({'fizz', 'buzz'}, 'fizzbuzz'),
+        (lambda x: x % 15 == 0, 'fizzbuzz'),
+        (lambda x: x % 3 == 0,  'fizz'),
+        (lambda x: x % 5 == 0,  'buzz'),
 ]
-fizz_buzz = GenericFizzBuzz(FIZZ_BUZZ_CONDITIONS, FIZZ_BUZZ_RULES)
+fizz_buzz = GenericFizzBuzz(FIZZ_BUZZ_RULES)
 
 def is_fib(n):
     """Definition as given in task"""
     x = 5 * n**2
     return gmpy.is_square(x + 4) or gmpy.is_square(x - 4)
 
-FLAMINGO_CONDITIONS = {'flamingo': lambda x: is_fib(x), **FIZZ_BUZZ_CONDITIONS}
-FLAMINGO_RULES = FIZZ_BUZZ_RULES + [
-    ({'flamingo'}, 'flamingo'),
-    ({'fizz', 'buzz', 'flamingo'}, 'pink flamingo')
-]
-flamingo = GenericFizzBuzz(FLAMINGO_CONDITIONS, FLAMINGO_RULES)
+FLAMINGO_RULES = [
+    (lambda x: x % 15 == 0 and is_fib(x), 'pink flamingo'),
+    (is_fib, 'flamingo')
+] + FIZZ_BUZZ_RULES
+
+flamingo = GenericFizzBuzz(FLAMINGO_RULES)
 
 def main():
     for i in range(1, 101):
